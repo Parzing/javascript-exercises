3e9912b77d7f75ad9518c5122afa151c23a26cb7
Parzing <jettae@schroff.com>
2025-08-14 23:49:36 -0700

    Finished 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, numReps) {
+    let finalStr = [];
+    for(let i = 0; i < numReps; i++) {
+        finalStr.push(str);
+    }
+    return finalStr.join("");
 };
 
 // Do not edit below this line
